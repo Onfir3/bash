dfc4d38430533dcb0744b04929ec7fb73507d314
wendeng <w526529650@outlook.com>
2018-06-11 14:13:30 +0800

    node.js更新

--- local/demo.js
+++ local/demo.js
@@ -1,13 +1,19 @@
-
 var fs = require('fs')
 
 var dirName = process.argv[2]//传的参数是从第二个开始的
+if (fs.existsSync(dirName)){
+    console.log('change file name')
+    process.exit(1)
+}else
+{
+
 fs.mkdirSync("./" + dirName) //相当于mkdir $1
 process.chdir("./" + dirName)//相当于cd $1
 fs.mkdirSync('css') //相当于mkdir css
 fs.mkdirSync('js') //相当于mkdir js
 fs.writeFileSync("index.html","<!DOCTYPE> <title>Hello<title> <h1>HI</h1>")
-fs.writeFileSync("css/style.css","hi {color:red}")
+fs.writeFileSync("css/style.css","h1 {color:red}")
 fs.writeFileSync("js/main.js","var string = 'Hello World' alert(string)")
-
+console.log('good job')
 process.exit(0)
+}
